046b64b4e18f76b49579350853c2e855cd78e16f
Dmitriy Don <dmitriysdon@gmail.com>
2021-08-31 11:57:26 +0300

    auth.create_jwt: add extra for any additional data #PLM-2752
    e.g. to keep session id & other data inside a JWT instead of backend's
    storage

--- src/smp/auth.py
+++ src/smp/auth.py
@@ -1,7 +1,7 @@
 import datetime
 
 
-def create_jwt(app_id, app_secret, user_id=None, session_id=None, scopes=None, duration=None):
+def create_jwt(app_id, app_secret, user_id=None, scopes=None, duration=None, extra_payload=None):
     import jwt
 
     if duration is None:
@@ -16,8 +16,8 @@ def create_jwt(app_id, app_secret, user_id=None, session_id=None, scopes=None, d
     if user_id is not create_jwt.undefined_user:
         payload['sub'] = user_id
 
-    if session_id is not None:
-        payload['sid'] = session_id
+    if extra_payload:
+        payload['extra'] = extra_payload
 
     return jwt.encode(payload, app_secret, algorithm='HS256')
 
