e415b1054a3e45aab3b91e3ea5c13d2ca6515d7c
Dmitriy Don <dmitriysdon@gmail.com>
2021-08-31 13:57:33 +0300

    auth.create_jwt: keep flat payload #PLN-1752

--- src/smp/auth.py
+++ src/smp/auth.py
@@ -7,17 +7,16 @@ def create_jwt(app_id, app_secret, user_id=None, scopes=None, duration=None, ext
     if duration is None:
         duration = datetime.timedelta(minutes=5)
 
-    payload = {
-        'iss': app_id,
-        'scp': scopes,
-        'exp': datetime.datetime.utcnow() + duration,
-    }
+    payload = dict(
+        iss=app_id,
+        scp=scopes,
+        exp=datetime.datetime.utcnow() + duration,
+        **(extra_payload or {})
+    )
 
     if user_id is not create_jwt.undefined_user:
+        assert 'sub' not in payload, 'got multiple values for payload key "sub"'
         payload['sub'] = user_id
-
-    if extra_payload:
-        payload['extra'] = extra_payload
 
     return jwt.encode(payload, app_secret, algorithm='HS256')
 
